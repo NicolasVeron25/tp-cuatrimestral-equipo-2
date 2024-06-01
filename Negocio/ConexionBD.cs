865d4c978681e1ece270401ef90109e7308f9b93
Pablo-Coll <112663594+Pablo-Coll@users.noreply.github.com>
2024-06-01 18:00:47 -0300

    Conexion a bd y creacion de capas dominio y negocio

---
+++ Negocio/ConexionBD.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class ConexionBD
+    {
+     
+        private SqlConnection _Conexion;
+        private SqlCommand _Comando;
+        private SqlDataReader _Lector;
+     
+        public SqlDataReader Lector
+        {
+            get { return _Lector; }
+        }
+
+        public ConexionBD()
+        {           
+            _Conexion = new SqlConnection(@"server =.\SQLEXPRESS;database=CODEMENTOR; integrated security = true"); 
+
+        }
+     
+        public void setQuery(string query)
+        {
+            _Comando.CommandType = System.Data.CommandType.Text;
+            _Comando.CommandText = query; 
+
+        }
+
+        public void ejecutarLectura()
+        {
+            _Comando.Connection = _Conexion; 
+            try
+            {
+                _Conexion.Open();
+                _Lector = _Comando.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+        public void ejecutarAccion()
+        {
+            _Comando.Connection = _Conexion;
+            try
+            {
+                _Conexion.Open();
+                _Comando.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public void cerrarConexion()
+        {
+            if (Lector != null) ;
+                _Conexion.Close();
+        }
+
+        public void setParametro(string name, object value) 
+        {
+            _Comando.Parameters.AddWithValue(name, value);
+        }
+    }
+}
+
+    
+
