f2d46d9182c2634d987a79e56b647b4ec905dde9
Diego Martín Santana <diego.santana@alumnos.frgp.utn.edu.ar>
2024-06-20 19:11:55 -0300

    Cerrar Session Admin

--- CodeMentor/AspxAdmin/AdminCursos.aspx.cs
+++ CodeMentor/AspxAdmin/AdminCursos.aspx.cs
@@ -13,30 +13,11 @@ namespace CodeMentor.AdminAspx
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-<<<<<<< HEAD
-            if (!IsPostBack)
-            {
-                CursosGestion curso = new CursosGestion();
-                List<Curso> cursoList = curso.Listado();
-                repRepetidor.DataSource = cursoList;
-                repRepetidor.DataBind();
-            }
-=======
             CursosGestion curso = new CursosGestion();
             List<Curso> cursoList = curso.Listado();
-            if (!IsPostBack)
-            {
             repRepetidor.DataSource = cursoList;
             repRepetidor.DataBind();
 
-            }
-
-        }
-
-        protected void ButtonModificar_Click(object sender, EventArgs e)
-        {
-            string valor = ((Button)sender).CommandArgument;
->>>>>>> 0091527ddf5d320be80ab0844da64a92ec114ddb
         }
     }
 }
